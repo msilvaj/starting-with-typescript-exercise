50b4a0c963bcec999681a9f32f7e45b94b675040
msilvaj <miqueias.ufpi@gmail.com>
2021-03-12 14:35:57 -0300

    Add an Interface to test types assignment

--- hello-world.js
+++ hello-world.js
@@ -1,4 +1,11 @@
+"use strict";
+exports.__esModule = true;
 function doIt() {
     var theText = "Hello World";
     alert(theText);
 }
+function logPoint(p) {
+    console.log(p.x + ", " + p.y);
+}
+var point = { x: 12, y: 26 };
+logPoint(point);
--- hello-world.ts
+++ hello-world.ts
@@ -5,3 +5,14 @@ function doIt(): void {
 
     alert(theText);
 }
+interface Point {
+ x: number;
+ y: number;
+}
+
+function logPoint(p: Point) {
+  console.log(`${p.x}, ${p.y}`);
+}
+
+const point = { x: 12, y: 26 };
+logPoint(point);
